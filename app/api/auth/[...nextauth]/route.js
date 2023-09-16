e94595a889a033788d0856e4e8d6136286674eea
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2023-09-16 22:02:00 +0530

    Feat: signin with Oauth with Database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import connectDB from '@/utils/database';
+import User from '@/models/userModel';
+
+connectDB();
 
 export const authOptions = {
   providers: [
@@ -11,8 +15,48 @@ export const authOptions = {
   pages: {
     signIn: '/signin',
   },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (account.type === 'oauth') {
+        return await signInWithOAuth({ account, profile });
+      }
+      return true;
+    },
+    async jwt({ token, trigger, session }) {
+      return token;
+    },
+    async session({ session, token }) {
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
+/**
+ *
+ * @param {*} param0
+ * @returns boolean after signin/signup user
+ */
+async function signInWithOAuth({ account, profile }) {
+  // check if there is a user already
+  const user = await User.findOne({ email: profile.email });
+
+  if (user) {
+    return true; // signin
+  }
+
+  // if not save user data in the database
+  const newUser = new User({
+    name: profile.name,
+    email: profile.email,
+    image: profile.picture,
+    provider: account.provider,
+  });
+
+  await newUser.save();
+
+  return true;
+}
--- models/userModel.js
+++ models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    name: {
+    email: {
       type: String,
       required: true,
       unique: true,
